48737dec0801203009ef4b718dfa736bf5da18ba
kaso <kaso@kasos-MacBook-Pro.local>
2018-06-26 00:15:20 -0700

    promise十道题结束

--- promise/main.js
+++ promise/main.js
@@ -96,3 +96,137 @@ question4 = () => {
 // 2
 // 结论
 // 每次调用.then/.catch都会返回一个新的promise，从而实现链式调用（传参）
+
+question5 = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('once')
+            resolve('success')
+        }, 1000);
+    })
+
+    const start = Date.now()
+
+    promise
+        .then(res => {
+            console.log(res, Date.now() - start)
+        })
+
+    promise
+        .then(res => {
+            console.log(res, Date.now() - start)
+        })
+}
+
+// 结果
+// once
+// success 1006
+// success 1008
+// 结论
+// Promise只构造一次
+// 其中的值可调用多次
+
+question6 = () => {
+    Promise.resolve()
+        .then(res => {
+            return new Error('error')
+        })
+        .then(res => {
+            console.log('then: ', res)
+        })
+        .catch(err => {
+            console.log('err', err)
+        })
+}
+
+// 结果
+// then:
+// Error: error
+// 结论
+// 在then或者catch过程中产生的error并不会抛出错误
+// 需要改写成 
+// throw new Error('erroe')
+// return Promise.reject(new Error('error'))
+// 原文
+// return new Error('error')
+
+question7 = () => {
+    const promise = Promise.resolve()
+        .then(() => {
+            return promise
+        })
+
+    promise.catch(console.error)
+}
+
+// 结果
+// TypeError: Chaining cycle detected for promise #<Promise>
+// 结论
+// 返回值不能为本身，否则换陷入死循环
+
+question8 = () => {
+    Promise.resolve(1)
+        .then(2)
+        .then(Promise.resolve(3))
+        .then(console.log)
+}
+
+// 结果
+// 1
+// 结论
+// .then/.catch期望传入为函数，非函数会发生值穿透
+
+question9 = () => {
+    Promise.resolve()
+        .then(function success(res) {
+            throw new Error('error')
+        }, function fail1(e) {
+            console.log('fail1: ', e)
+        })
+        .catch(function fail2(e) {
+            console.log('fail2: ', e)
+        })
+}
+
+// 结果
+// fail2:
+// Error:error
+// 结论
+// .then可以接受两个参数（成功-res,失败-err）
+// .catch是第二个参数的简便写法
+
+// 注意：用.then捕获错误无法知道前一个参数的错误而.catch可以
+
+question10 = () =>{
+    process.nextTick( ()=>{
+        console.log('next tick')
+    })
+
+    Promise.resolve()
+    .then(() =>{
+        console.log('then')
+    })
+
+    setImmediate(()=>{
+        console.log('setImmediate')
+    })
+
+    console.log('end')
+}
+
+// 结果
+// end
+// next tick
+// then
+// setImmediate
+// 结论
+// microtack:process,promise.then
+// macrotack:setImmediate
+// check阶段执行macroctask
+// 循环阶段执行microtask
+
+// 总结
+// 十道题全部结束了，其中核心的可能为
+// 1.数据传输
+// 2.错误抛出
+// 3.执行顺序
