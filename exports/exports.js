7abbfec5739771a8f7b6009983a0cddb1d40593d
kaso <kaso@kasos-MacBook-Pro.local>
2018-06-28 01:57:15 -0700

    cc

--- exports/exports.js
+++ exports/exports.js
@@ -14,3 +14,5 @@ const a = require()
 // 结论：exports是module.exports的引用，export是个局部变量，module是个全局变量
 //      引用知识点，见js引用
 //      exports => {} <=module.exports
+
+//      如果一个新的值被赋值给 exports，它就不再绑定到 module.exports
